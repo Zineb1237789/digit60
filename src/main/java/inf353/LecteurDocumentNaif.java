18e1d41cc311b2b7ebeacf2024555c7a89285962
Zineb El Aouad <zineb.el-aouad@etu.univ-grenoble-alpes.fr>
2023-12-17 21:34:06 +0100

    Update src/main/java/inf353/LecteurDocumentNaif.java

--- src/main/java/inf353/LecteurDocumentNaif.java
+++ src/main/java/inf353/LecteurDocumentNaif.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 
-public class LecteurDocumentNaif{
+public class LecteurDocumentNaif<T>{
     public String mot;
     public BufferedReader lect;
     public int caractere;
